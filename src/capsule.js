6f4e59fe9ab8c9d7bf95a9251e9efb015f6bbeec
DaveP <hello@davep.co>
2020-11-25 18:01:51 +0000

    feat: get all guilds, channel, & pins

--- src/capsule.js
+++ src/capsule.js
@@ -10,4 +10,27 @@ bot.on('error', e => core.log.error(e));
 bot.on('warn', w => core.log.warn(w));
 bot.on('ready', () => {
   core.log.info('ready');
+
+  bot.guilds.cache.each(({ channels }) => {
+    channels.cache.each(async ({ type, messages }) => {
+      if (type !== 'text') {
+        return;
+      }
+
+      try {
+        const pins = await messages.fetchPinned();
+
+        const date = core.date(new Date('2017-09-17T19:29:28.769Z'));
+
+        // console.log(date);
+        pins
+          .filter(message => core.date(message.createdAt) === date)
+          .each(message => {
+            console.log(message.content);
+          });
+      } catch (e) {
+        core.log.error(e);
+      }
+    });
+  });
 });
