ea330e2ec8f2cc9485a8a94b148e89d5a1f1fd7d
DaveP <hello@davep.co>
2020-11-28 00:35:03 +0000

    refac: split logic out into helper functions

--- src/capsule.js
+++ src/capsule.js
@@ -11,52 +11,73 @@ bot.on('warn', w => core.log.warn(w));
 bot.on('ready', () => {
   core.log.info('ready');
 
+  bot.guilds.cache.each(async ({ channels }) => {
+    const archive = (await getArchive(channels)) || [];
+
+    archive
+      .flat()
+      .sort((a, b) => a.year - b.year)
+      .forEach(archive => {
+        console.log(
+          `${archive.year} year${
+            archive.year > 1 ? 's' : ''
+          } ago, today there were ${archive.messages.length} pins 📌`
+        );
+      });
+  });
+});
+
+/**
+ * Get a full archive of pins from a Server
+ * @param {GuildChannelManager} channels
+ */
+async function getArchive({ cache }) {
+  return cache.reduce(async (pins, { type, messages }) => {
+    let all = await pins;
+
+    if (type === 'text') {
+      const archive = (await getPins(messages)) || {};
+
+      if (Object.entries(archive).length !== 0) {
+        all.push(archive);
+      }
+    }
+
+    return all;
+  }, Promise.resolve([]));
+}
+
+/**
+ * Get a full archive of pins, grouped by year
+ * @param {MessageManager} messages
+ */
+async function getPins(messages) {
   const date = new Date('2017-09-17T19:29:28.769Z');
   const compare = core.date(date);
 
-  bot.guilds.cache.each(async ({ channels }) => {
-    const x = await channels.cache.reduce(
-      async (allPromise, { type, messages }) => {
-        let all = await allPromise;
+  try {
+    const pins = await messages.fetchPinned();
 
-        if (type === 'text') {
-          try {
-            const pins = await messages.fetchPinned();
+    return pins.reduce((pins, pin) => {
+      if (core.date(pin.createdAt) === compare) {
+        const year = new Date().getFullYear() - pin.createdAt.getFullYear();
 
-            const archive = pins.reduce((pins, pin) => {
-              if (core.date(pin.createdAt) === compare) {
-                const year =
-                  new Date().getFullYear() - pin.createdAt.getFullYear();
+        let current = pins.find(p => p.year === year);
 
-                let current = pins.find(p => p.year === year);
+        if (!current) {
+          current = { year, messages: [] };
 
-                if (!current) {
-                  current = { year, messages: [] };
-
-                  pins.push(current);
-                }
-
-                current.messages.push(pin);
-              }
-
-              return pins;
-            }, []);
-
-            if (Object.entries(archive).length !== 0) {
-              all.push(archive);
-            }
-
-            // `${year} year${year > 1 ? 's' : ''} ago, today`,
-          } catch (e) {
-            core.log.error(e);
-          }
+          pins.push(current);
         }
 
-        return all;
-      },
-      Promise.resolve([])
-    );
+        current.messages.push(pin);
+      }
 
-    console.log(x.flat().sort((a, b) => a.year - b.year));
-  });
-});
+      return pins;
+    }, []);
+  } catch (e) {
+    core.log.error(e);
+
+    return {};
+  }
+}
