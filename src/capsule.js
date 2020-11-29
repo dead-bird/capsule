e5f103a70813855448e552303b3a3fb8041a8573
DaveP <hello@davep.co>
2020-11-29 21:59:59 +0000

    feat: added first time setup

--- src/capsule.js
+++ src/capsule.js
@@ -1,5 +1,6 @@
 import memories from './modules/memories';
 import { Client } from 'discord.js';
+import setup from './modules/setup';
 import dotenv from 'dotenv/config';
 import core from './modules/core';
 
@@ -7,6 +8,7 @@ const bot = new Client();
 
 bot.login(process.env.TOKEN).catch(e => core.log.error(e));
 
+bot.on('guildCreate', guild => setup(guild));
 bot.on('error', e => core.log.error(e));
 bot.on('warn', w => core.log.warn(w));
 bot.on('ready', () => {
---
+++ src/modules/setup.js
@@ -0,0 +1,42 @@
+import core from './core';
+
+// Create a #memories channel when we join a server
+export default function run(guild) {
+  guild.me.hasPermission('ADMINISTRATOR')
+    ? fullSetup(guild)
+    : setupWithoutAdmin(guild);
+}
+
+async function fullSetup(guild) {
+  // Try and grab #memories
+  let channel = core.findTargetChannel(guild.channels);
+
+  // Create #memories channel & deny everyone access (admins will override this)
+  if (!channel) {
+    channel = await guild.channels.create(core.targetChannel, {
+      topic: 'A channel for your past pins ',
+      permissionOverwrites: [{ id: guild.id, deny: ['VIEW_CHANNEL'] }],
+    });
+  }
+
+  channel.send(`Hello 👋🏻 I'll be posting memories here`);
+}
+
+function setupWithoutAdmin(guild) {
+  // If we don't have perms to create #memories, try and find a #general channel
+  const channel = guild.channels.cache.find(
+    c => c.type === 'text' && c.permissionsFor(guild.me).has('SEND_MESSAGES')
+  );
+
+  if (channel) {
+    // Message #general and ask for a channel
+    channel.send(
+      `To get memories, please create a #${core.targetChannel} text channel and give me admin access 🙏`
+    );
+  } else {
+    // Message the owner and ask for a channel
+    guild.owner.send(
+      `To get memories, please create a #${core.targetChannel} text channel in your ${guild.name} server and give me admin access 🙏`
+    );
+  }
+}
